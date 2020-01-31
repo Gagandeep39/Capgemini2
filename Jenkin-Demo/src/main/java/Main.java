504d43fe6487655e54fec4f0eb4279f1e1e7aae7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-01-31 15:41:58 +0530

    Add files via upload

---
+++ Jenkin-Demo/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee employee  = new Employee();
+		employee.setId(101);
+		employee.setName("Gagan");
+		System.out.println(employee.getId() + " : " + employee.getName());
+	}
+}
